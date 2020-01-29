74a3adb384bce970ed6ea07b5c976ac0ddb3c045
ScholarOftheFirstSin <45855539+ScholarOftheFirstSin@users.noreply.github.com>
2020-01-29 14:00:37 -0500

    Add Park!

--- 01-python/outdoors/park.py
+++ 01-python/outdoors/park.py
@@ -1,3 +1,23 @@
-def draw_park():
-    print("park not found")
-    return
+def draw_park(length = 5, width = 5):
+	"""
+	Print the park with specific length and width
+
+	Parameters:
+
+	Length (int) : the length of the park.
+	Width (int) : the width of the park.
+
+	Returns:
+	No return.
+
+	"""
+	if length < 0 or width < 0:
+		raise ValueError("Please input valid length and width")
+	for _ in range (length):
+		print()
+		for _ in range (width):
+			print("|-|", end = "")
+
+	if length != 0:
+		print()
+
